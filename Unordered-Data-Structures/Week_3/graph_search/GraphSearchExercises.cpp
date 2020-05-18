993efbca9b9235997e830332e616664cd8c1b4df
Drew Hanover <djhanove@mtu.edu>
2020-05-18 11:12:45 -0600

    Passing all tests in both ./main and ./test. Finished exercise 3 by modifying the graphBFS function to accomodate puzzle state types.

--- Unordered-Data-Structures/Week_3/graph_search/GraphSearchExercises.cpp
+++ Unordered-Data-Structures/Week_3/graph_search/GraphSearchExercises.cpp
@@ -539,7 +539,7 @@ std::list<PuzzleState> puzzleBFS(const PuzzleState& start, const PuzzleState& go
     // We'll need to loop over the neighbors that are the points adjacent to curState.
     // We need a collection of neighbors we're going to loop over.
     
-    auto neighbors = {start}; // Change this! This line is totally wrong.
+    auto neighbors = curState.getAdjacentStates(); // Change this! This line is totally wrong.
 
     // Hint: Look at PuzzleState.h
     // =====================================================================
@@ -549,7 +549,7 @@ std::list<PuzzleState> puzzleBFS(const PuzzleState& start, const PuzzleState& go
       // ==================================================================
       // TODO: Your code here!
       // Check whether the neighbor has already been visited.
-      bool neighborWasAlreadyVisited = false; // Change this...
+      bool neighborWasAlreadyVisited = visitedSet.count(neighbor); // Change this...
       // ==================================================================
 
       if (!neighborWasAlreadyVisited) {
@@ -561,13 +561,14 @@ std::list<PuzzleState> puzzleBFS(const PuzzleState& start, const PuzzleState& go
         // since curState has just led to the discovery of this neighbor for
         // the first time.
         // ...
+        pred[neighbor] = curState;
 
         // Add neighbor to the visited set.
         // ...
-
+        visitedSet.insert(neighbor);
         // Push neighbor into the exploration queue.
         // ...
-
+        exploreQ.push(neighbor);
         // ================================================================
 
         dist[neighbor] = dist[curState]+1;
